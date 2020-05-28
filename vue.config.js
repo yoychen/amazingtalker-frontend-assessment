b5ba131f966be07396b02a314fd8f5ab46f1563e
yoychen <yui12327@gmail.com>
2020-05-28 17:41:57 +0000

    Use GitHub Actions to test and deploy to gh-page

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/amazingtalker-frontend-assessment/'
+    : '/',
   devServer: {
     proxy: 'https://api.amazingtalker.com/v1/guest/teachers/amy-estrada',
   },
